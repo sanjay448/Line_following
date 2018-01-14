323027c7a731fae494c0cbcfb13bb4d2ca8b702a
Vaibhav Garg <vaibhav.garg771@gmail.com>
2018-01-14 17:04:33 +0530

    Update line_follow.ino

--- line_follow.ino
+++ line_follow.ino
@@ -7,10 +7,10 @@
 //#define mlen
 //#define mren
 
-int mlpwm=0,mrpwm=0,mls=220,mrs=220;
+int mlpwm=0,mrpwm=0,mls=100,mrs=100;
 int reading[7];
 int dread[7];
-int perr=0, derr=0, corr=0, kp=15, kd=10, sen_thr=450,sum=0,err=0,wsum=0;
+int perr=0, derr=0, corr=0, kp=25, kd=18, sen_thr=450,sum=0,err=0,wsum=0;
 
 void setup()
 {
@@ -40,13 +40,14 @@ void setup()
       if(reading[l]<sen_thr) dread[l]=0;
       else dread[l]=1;
       }
+      if(reading[0]>200) dread[0]=1;
       }
     
     void calc_error(){
       sum=0;
     for(int l=0; l<7;l++)
       sum=sum+dread[l];
-     int wsum=7*dread[0]+4*dread[1]+dread[2]-dread[4]-4*dread[5]-7*dread[6];
+     int wsum=7*dread[0]+5*dread[1]+dread[2]-dread[4]-5*dread[5]-7*dread[6];
     err=wsum/sum;
       }
     
@@ -58,11 +59,30 @@ void setup()
     
         mrpwm=mrs+corr;
          mlpwm=mls-corr;
-         
       if(mlpwm>254) mlpwm=254;
-      else if(mrpwm>254) mrpwm=254;
-      else if(mlpwm<0) mlpwm=0;
-      else if(mrpwm<0) mrpwm=0;
+      if(mrpwm>254) mrpwm=254;
+      if(mlpwm<0) {
+        mlpwm=(-1)*mlpwm;
+        digitalWrite(mlf,LOW);
+      digitalWrite(mrf,HIGH);
+      digitalWrite(mlb,HIGH);
+      digitalWrite(mrb,LOW);
+      
+      analogWrite(mlp,mlpwm);
+      analogWrite(mrp,mrpwm);
+      }
+      if(mrpwm<0) {
+        mrpwm=(-1)*mrpwm;
+        if(mrpwm>254) mrpwm=254;
+        digitalWrite(mrf,LOW);
+      digitalWrite(mlf,HIGH);
+      digitalWrite(mrb,HIGH);
+      digitalWrite(mlb,LOW);
+      
+      analogWrite(mlp,mlpwm);
+      analogWrite(mrp,mrpwm);
+        }
+      if(mlpwm>0 && mrpwm>0){
       digitalWrite(mlf,HIGH);
       digitalWrite(mrf,HIGH);
       digitalWrite(mlb,LOW);
@@ -70,6 +90,7 @@ void setup()
       
       analogWrite(mlp,mlpwm);
       analogWrite(mrp,mrpwm);
+      }
       }
 
 void line_follow(){
@@ -81,5 +102,4 @@ void line_follow(){
 void loop()
 {
  line_follow();
- delay(10);
  }      
